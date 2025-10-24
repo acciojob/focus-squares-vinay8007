3436fcdba4371b4644ba1553403049e3a629a2e9
vinay8007 <161361880+vinay8007@users.noreply.github.com>
2025-10-24 22:52:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ const squares = document.querySelectorAll(".square");
 squares.forEach((square, index) => {
   square.addEventListener("mouseenter", () => {
     squares.forEach((sq, i) => {
-      if (i !== index) {
-        sq.style.backgroundColor = "#6F4E37"; // Coffee
+      if (i === index) {
+        sq.style.backgroundColor = "#E6E6FA"; 
+      } else {
+        sq.style.backgroundColor = "#6F4E37"; 
       }
     });
   });
 
   square.addEventListener("mouseleave", () => {
     squares.forEach(sq => {
-      sq.style.backgroundColor = "#E6E6FA"; // Reset to Lavender
+      sq.style.backgroundColor = "#E6E6FA";
     });
   });
 });
